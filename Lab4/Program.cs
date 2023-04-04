877f9cba857eb070153650654df876f4945f066e
Twinker25 <shashokd@gmail.com>
2023-04-04 19:01:06 +0300

    Add task #1

--- Lab4/Program.cs
+++ Lab4/Program.cs
@@ -4,7 +4,25 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //1
+            Console.WriteLine("Task 1");
+            string input;
+            int num;
+            try
+            {
+                Console.Write("Enter number: ");
+                input = Console.ReadLine();
+                num = int.Parse(input);
+                Console.WriteLine($"Your number: {num}");
+            }
+            catch (OverflowException)
+            {
+                Console.WriteLine("This number is maximize than type int");
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Incorrect format of number");
+            }
         }
     }
 }
